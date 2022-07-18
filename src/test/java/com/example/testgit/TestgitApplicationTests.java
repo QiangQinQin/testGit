53d4e1d649d2be430e25c86fe06ef5c750912376
QiangQinQin <2901422401@qq.com>
2022-07-19 00:01:28 +0800

    测试springboot的@Data注解

--- src/test/java/com/example/testgit/TestgitApplicationTests.java
+++ src/test/java/com/example/testgit/TestgitApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.testgit;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +10,7 @@ class TestgitApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("hello  world");
+
     }
 
 }
